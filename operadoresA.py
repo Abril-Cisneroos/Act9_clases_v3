15725f7da70d9c58e478e052120407170fec1a59
Abril-Cisneroos <a.22308051281162@cbtis128.edu.mx>
2024-09-20 09:50:35 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,64 @@
+print("Act 9 clases v2")
+print("Frida Cisneros Mat:22308051281162")
+# zona de class
+
+class Operadores1162:
+    #zona de funciones
+    def suma1162(self,F,C):
+     s1162=F+C
+     print(f"La suma de {F} + {C} = {s1162}")
+     
+    def resta1162(self,F,C):
+     r1162=F-C
+     print(f"La resta de {F} - {C} = {r1162}")
+     
+    def mult1162(self,F,C):
+      m1162=F*C
+      print(f"La multiplicacin de {F} * {C} = {m1162}")
+      
+    def div1162(self,F,C):
+      d1162=F/C
+      print(f"La divicion de {F} / {C} = {d1162}")
+      
+    def mod1162(self,F,C):
+      mod1162=F%C
+      print(f"El modulo de {F} % {C} = {mod1162}")
+      
+    def ex1162(self,F,C):
+      ex1162=F**C
+      print(f"El exponente de {F} ** {C} = {ex1162}")
+      
+    def cos1162(self,F,C):
+      c1162=F//C
+      print(f"El cosiente {F} // {C} = {c1162}")
+      
+      
+     
+     
+# zona de creacion del objeto 
+operacis=Operadores1162()
+
+
+
+#zona de uso de objetos   
+print("Funcion suma") 
+operacis.suma1162(5,4) 
+print("Funcion resta")
+operacis.resta1162(10,4)
+print("Funcion multiplicacion")
+operacis.mult1162(2,2)
+print("Funcion divicion")
+operacis.div1162(10,2)
+print("Funcion modulo")
+operacis.mod1162(5,2)
+print("Funcion exponente")
+operacis.ex1162(2,2)
+print("Funcion cociente")
+operacis.cos1162(13,2)
+
+
+
+
+
+
+
